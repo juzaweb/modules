915fe8b0aa3332c1455cd51d59ecdc0ee85e0016
Juzaweb <dangtheanh16@gmail.com>
2023-09-02 13:58:00 +0700

    :construction: Datatable Element builder

--- modules/CMS/Support/Element/Elements/DataTable.php
+++ modules/CMS/Support/Element/Elements/DataTable.php
@@ -11,18 +11,92 @@
 namespace Juzaweb\CMS\Support\Element\Elements;
 
 use Illuminate\Support\Arr;
-use Juzaweb\CMS\Abstracts\BaseDataTable;
+use Illuminate\Support\Facades\Crypt;
+use Juzaweb\CMS\Abstracts\Action;
 use Juzaweb\CMS\Support\Element\Interfaces\Element;
 use Juzaweb\CMS\Support\Element\Traits\HasClass;
 use Juzaweb\CMS\Support\Element\Traits\HasId;
 
-class DataTable extends BaseDataTable implements Element
+class DataTable implements Element
 {
     use HasClass, HasId;
 
     protected string $element = 'data-table';
 
     protected string $class = 'table data-table';
+
+    /**
+     * Number of items per page.
+     *
+     * @var int
+     */
+    protected int $perPage = 10;
+
+    /**
+     * Name of the attribute used for sorting.
+     *
+     * @var string
+     */
+    protected string $sortName = 'id';
+
+    /**
+     * Sorting order (asc or desc).
+     *
+     * @var string
+     */
+    protected string $sortOder = 'desc';
+
+    /**
+     * Additional parameters.
+     *
+     * @var array
+     */
+    protected array $params = [];
+
+    /**
+     * URL for fetching data.
+     *
+     * @var string|null
+     */
+    protected ?string $dataUrl = null;
+
+    /**
+     * URL for performing actions.
+     *
+     * @var string|null
+     */
+    protected ?string $actionUrl = null;
+
+    /**
+     * Array of characters to escape.
+     *
+     * @var array
+     */
+    protected array $escapes = [];
+
+    /**
+     * Flag indicating whether searching is enabled.
+     *
+     * @var bool
+     */
+    protected bool $searchable = true;
+
+    /**
+     * Current URL.
+     *
+     * @var string|null
+     */
+    protected ?string $currentUrl = null;
+
+    protected array $columns = [];
+
+    protected array $actions = [];
+
+    protected array $bulkActions = [];
+
+    protected array $searchFields = [];
+
+    protected array $rowActions = [];
 
     public function __construct(array $configs)
     {
@@ -33,15 +107,132 @@ class DataTable extends BaseDataTable implements Element
         }
     }
 
+    public function perPage(int $perPage): static
+    {
+        $this->perPage = $perPage;
+
+        return $this;
+    }
+
+    public function sortName(string $sortName): static
+    {
+        $this->sortName = $sortName;
+
+        return $this;
+    }
+
+    public function sortOder(string $sortOder): static
+    {
+        $this->sortOder = $sortOder;
+
+        return $this;
+    }
+
+    public function params(array $params): static
+    {
+        $this->params = $params;
+
+        return $this;
+    }
+
+    public function dataUrl(string $dataUrl): static
+    {
+        $this->dataUrl = $dataUrl;
+
+        return $this;
+    }
+
+    public function actionUrl(string $actionUrl): static
+    {
+        $this->actionUrl = $actionUrl;
+
+        return $this;
+    }
+
+    public function escapes(array $escapes): static
+    {
+        $this->escapes = $escapes;
+
+        return $this;
+    }
+
+    public function searchable(bool $searchable): static
+    {
+        $this->searchable = $searchable;
+
+        return $this;
+    }
+
+    public function currentUrl(string $currentUrl): static
+    {
+        $this->currentUrl = $currentUrl;
+
+        return $this;
+    }
+
+    public function columns(array $columns): static
+    {
+        $this->columns = $columns;
+
+        return $this;
+    }
+
+    public function actions(array $actions): static
+    {
+        $this->actions = $actions;
+
+        return $this;
+    }
+
+    public function bulkActions(array $bulkActions): static
+    {
+        $this->bulkActions = $bulkActions;
+
+        return $this;
+    }
+
+    public function searchFields(array $searchFields): static
+    {
+        $this->searchFields = $searchFields;
+
+        return $this;
+    }
+
+    public function rowActions(array $rowActions): static
+    {
+        $this->rowActions = $rowActions;
+
+        return $this;
+    }
+
     public function toArray(): array
     {
-        $parent = parent::toArray();
-
-        return array_merge($parent, Arr::only(get_object_vars($this), ['class', 'element', 'id']));
+        return [
+            'columns' => $this->columns,
+            'actions' => $this->actions,
+            'params' => $this->params,
+            'searchFields' => $this->searchFields,
+            'perPage' => $this->perPage,
+            'sortName' => $this->sortName,
+            'sortOder' => $this->sortOder,
+            'dataUrl' => $this->dataUrl,
+            'actionUrl' => $this->actionUrl,
+            'escapes' => $this->escapes,
+            'searchable' => $this->searchable,
+            'searchFieldTypes' => $this->getSearchFieldTypes(),
+            'table' => Crypt::encryptString(static::class),
+            'uniqueId' => $this->getId(),
+            ...Arr::only(get_object_vars($this), ['class', 'element', 'id'])
+        ];
     }
 
     public function render(): string
     {
         return '';
+    }
+
+    protected function getSearchFieldTypes(): array
+    {
+        return apply_filters(Action::DATATABLE_SEARCH_FIELD_TYPES_FILTER, []);
     }
 }
