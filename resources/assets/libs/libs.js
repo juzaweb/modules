597b31885c6c7fce4004970f83025ead61ab3def
Juzaweb <dangtheanh16@gmail.com>
2024-04-30 22:06:14 +0700

    :+1: CMS Echo

--- resources/assets/libs/libs.js
+++ resources/assets/libs/libs.js
@@ -1,4 +1,6 @@
 import $ from "jquery";
+//import Echo from 'laravel-echo';
+
 window.$ = window.jQuery = $;
 
 window.Popper = require('popper.js').default;
@@ -6,3 +8,17 @@ window.Popper = require('popper.js').default;
 import 'bootstrap';
 import Chart from 'chart.js/auto';
 window.Chart = Chart;
+
+// window.Pusher = require('pusher-js');
+
+// const CMSEcho = new Echo({
+//     broadcaster: 'pusher',
+//     key: 'xxx',
+//     cluster: 'mt1',
+//     forceTLS: true,
+// });
+
+// const channel = CMSEcho.channel('notification');
+// channel.listen('.new-notification', function (data) {
+//     alert(data?.message);
+// });
